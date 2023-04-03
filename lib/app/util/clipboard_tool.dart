cb15fce1ace4d39c6bcea8b6831ccd9e1a19882a
Dolin <366688603@qq.com>
2023-04-03 20:29:58 +0800

    + 剪贴板、字符串、吐司工具类

---
+++ lib/app/util/clipboard_tool.dart
@@ -0,0 +1,34 @@
+//复制粘贴
+import 'package:dolin_demo_flutter/app/util/toast.dart';
+import 'package:flutter/services.dart';
+
+/// 剪切板工具
+class ClipboardTool {
+  // 复制内容
+  static setData(String data) {
+    if (data != '') {
+      Clipboard.setData(ClipboardData(text: data));
+    }
+  }
+
+  // 复制内容
+  static setDataToast(String data) {
+    if (data != '') {
+      Clipboard.setData(ClipboardData(text: data));
+      showToast('复制成功');
+    }
+  }
+
+  // 复制内容
+  static setDataToastMsg(String data, {String toastMsg = '复制成功'}) {
+    if (data != '') {
+      Clipboard.setData(ClipboardData(text: data));
+      showToast(toastMsg);
+    }
+  }
+
+  // 获取内容
+  static Future<ClipboardData?> getData() {
+    return Clipboard.getData(Clipboard.kTextPlain);
+  }
+}
