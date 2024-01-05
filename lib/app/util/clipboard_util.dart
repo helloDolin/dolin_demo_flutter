af4b501f8202c5994df3bb79a25ac9974a54fa46
helloDolin <366688603@qq.com>
2024-01-05 20:25:29 +0800

    update

--- lib/app/util/clipboard_util.dart
+++ lib/app/util/clipboard_util.dart
@@ -1,17 +1,23 @@
-//复制粘贴
-import 'package:dolin/app/util/toast_util.dart';
-import 'package:flutter/services.dart';
+// ignore: dangling_library_doc_comments
+/// @author shaolin
+/// @email 366688603@qq.com
+/// @create date 2024-01-02 15:29:53
+/// @modify date 2024-01-02 15:29:53
+/// @desc [剪贴板工具]
 
-/// 剪切板工具
+import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+/// 剪贴板工具
 class ClipboardUtil {
-  // 复制内容
+  /// 复制内容-无 toast 提示
   static void setData(String data) {
     if (data != '') {
       Clipboard.setData(ClipboardData(text: data));
     }
   }
 
-  // 复制内容
+  // 复制内容-toast 提示复制成功
   static void setDataToast(String data) {
     if (data != '') {
       Clipboard.setData(ClipboardData(text: data));
@@ -19,7 +25,7 @@ class ClipboardUtil {
     }
   }
 
-  // 复制内容
+  // 复制内容- 自定义 toast 内容
   static void setDataToastMsg(String data, {String toastMsg = '复制成功'}) {
     if (data != '') {
       Clipboard.setData(ClipboardData(text: data));
@@ -27,8 +33,17 @@ class ClipboardUtil {
     }
   }
 
-  // 获取内容
+  /// 获取内容
   static Future<ClipboardData?> getData() {
     return Clipboard.getData(Clipboard.kTextPlain);
+  }
+
+  /// Fluttertoast
+  static void showToast(String text) {
+    Fluttertoast.showToast(
+      msg: text,
+      toastLength: Toast.LENGTH_LONG,
+      gravity: ToastGravity.CENTER,
+    );
   }
 }
